e02d4f85661a8135535ecb92cc4035c83d157a0a
Andrea Della Corte <andrea@dellacorte.me>
2025-04-21 13:39:11 +0100

    feat: setup script improv

--- setup.sh
+++ setup.sh
@@ -28,6 +28,29 @@ fi
 
 wandb login
 
+# Install gh CLI if not installed
+if ! command -v gh &> /dev/null; then
+    echo "gh CLI not found. Installing..."
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        (type -p wget >/dev/null || (apt update && apt-get install wget -y)) \
+            && mkdir -p -m 755 /etc/apt/keyrings \
+                && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+                && cat $out | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+            && chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+            && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+            && apt update \
+            && apt install gh -y
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install gh
+    else
+        echo "Unsupported OS. Please install gh CLI manually."
+    fi
+else
+    echo "gh CLI is already installed."
+fi
+
+gh auth login
+
 # if virtual environment is not active, provide instructions
 if [[ "$CURRENT_VIRTUAL_ENV" == "" ]]; then
     echo "Virtual environment is not active. Please activate it using:"
---
+++ src/utils/data_extraction.py
@@ -0,0 +1,1 @@
+microsoft/ms_marco
